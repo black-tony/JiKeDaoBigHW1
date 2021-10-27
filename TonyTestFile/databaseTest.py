e9493a3b2094e2d3f398d0061d90fa7e0f072d2e
black-tony <58112210+black-tony@users.noreply.github.com>
2021-10-28 00:13:53 +0800

    数据库工具集

--- TonyTestFile/databaseTest.py
+++ TonyTestFile/databaseTest.py
@@ -1,5 +1,5 @@
+import string
 import traceback
-
 import pymysql
 
 HOST_CONST = "localhost"
@@ -7,6 +7,7 @@ USER_CONST = "root"
 PORT_CONST = 3306
 PASSWORD_CONST = "Gz8ymJMXfbr#3*5"
 DATABASE_CONST = "test_db"
+TABLE_CONST = "employee"
 
 
 class MysqlUtil(object):
@@ -20,6 +21,22 @@ class MysqlUtil(object):
         self.db = pymysql.connect(host=host, port=port, user=user, password=password, db=database)
         self.cursor = self.db.cursor(cursor=pymysql.cursors.DictCursor)
 
+    @staticmethod
+    def __phraseSQL(table, fields, condition="NULL"):
+        sql = "SELECT "
+        haveQuote = 0
+        for i in fields:
+            if haveQuote == 0:
+                haveQuote = 1
+            else:
+                sql += ', '
+            sql += i
+        sql += "FROM " + table
+        if condition != "NULL":
+            sql += f'WHERE "{condition}"'
+        sql += ';'
+        return sql
+
     def ChangeDatabase(self, databaseName):
         self.db.close()
         self.cursor.close()
@@ -31,57 +48,97 @@ class MysqlUtil(object):
         self.db = pymysql.connect(host=host, port=port, user=user, password=password, db=database)
         self.cursor = self.db.cursor(cursor=pymysql.cursors.DictCursor)
 
-    def insert(self, sql):
+    def insert(self, table=TABLE_CONST, **fields_and_vals):
+        sql = "INSERT INTO " + table
+        sql += '('
+        haveQuote = 0
+        for firstPart, secondPart in fields_and_vals:
+            if haveQuote == 0:
+                haveQuote = 1
+            else:
+                sql += ', '
+            sql += f"{firstPart}"
+        sql += ') VALUES ('
+        haveQuote = 0
+        for firstPart, secondPart in fields_and_vals:
+            if haveQuote == 0:
+                haveQuote = 1
+            else:
+                sql += ', '
+            if type(secondPart) == string:
+                sql += f'"{secondPart}"'
+            else:
+                sql += f"{secondPart}"
+
+        sql += ');'
         try:
             self.cursor.execute(sql)
             self.db.commit()
-        except:
+        except pymysql.DatabaseError:
             traceback.print_exc()
             self.db.rollback()
         finally:
             self.db.close()
 
-    def fetchone(self, sql):
+    def fetchone(self, table, condition="NULL", *fields):
         # return with list<dictionary>
+        sql = self.__phraseSQL(table=table, condition=condition, fields=fields)
+
         result = list()
         try:
             self.cursor.execute(sql)
             result = self.cursor.fetchone()
-        except:
+        except pymysql.DatabaseError:
             traceback.print_exc()
             self.db.rollback()
         finally:
             self.db.close()
         return result
 
-    def fetchall(self, sql):
+    def fetchall(self, table, condition="NULL", *fields):
         # return with list<dictionary>
-        result = list()
+        sql = self.__phraseSQL(table=table, condition=condition, fields=fields)
+        results = list()
         try:
             self.cursor.execute(sql)
             results = self.cursor.fetchall()
-        except:
+        except pymysql.DatabaseError:
             traceback.print_exc()
             self.db.rollback()
         finally:
             self.db.close()
         return results
 
-    def delete(self, sql):
+    def delete(self, table, condition):
+        sql = f"DELETE FROM {table} WHERE {condition}"
         try:
             self.cursor.execute(sql)
             self.db.commit()
-        except:
+        except pymysql.DatabaseError:
             traceback.print_exc()
             self.db.rollback()
         finally:
             self.db.close()
 
-    def update(self, sql):
+    def update(self, condition="NULL", table=TABLE_CONST, **fields_and_vals):
+        sql = f'UPDATE {table} SET '
+        haveQuote = 0
+        for firstVal, secondVal in fields_and_vals:
+            if haveQuote == 0:
+                haveQuote = 1
+            else:
+                sql += ', '
+            sql += f"{firstVal}="
+            if type(secondVal) == string:
+                sql += f'"{secondVal}"'
+            else:
+                sql += f"{secondVal}"
+        if condition != "NULL":
+            sql += condition
         try:
             self.cursor.execute(sql)
             self.db.commit()
-        except:
+        except pymysql.DatabaseError:
             traceback.print_exc()
             self.db.rollback()
         finally:
